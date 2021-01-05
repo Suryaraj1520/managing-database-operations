2ddc2e63e7c230cac324c45a65bb3cd391302bd2
Suryaraj1520 <73297356+Suryaraj1520@users.noreply.github.com>
2021-01-05 14:39:36 +0530

    created route
    created new route and added new model
    run
    > py .\manage.py makemigrations
    > py .\manage.py migrate

--- database/college/models.py
+++ database/college/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
+class College(models.Model):
+
+        id = models.BigAutoField(
+        primary_key=True,
+        )
+
+        name = models.CharField(
+            max_length=200,
+            unique=True,
+            blank=False,
+            null=False,
+            verbose_name='College Name',
+            validators=[
+            MinLengthValidator(10)
+            ]
+        )
+
+        code = models.IntegerField(
+            verbose_name='College Code',
+            blank=True,
+            validators=[
+                MinLengthValidator(6)
+            ]
+        )
+    
+        chairman = models.CharField(
+            max_length=150,
+            blank=False,
+            verbose_name='Chairman'
+        )
+
+        established_on = models.DateField(
+            auto_now_add=True,
+            verbose_name='Established Name'
+        )
+    
--- database/college/views.py
+++ database/college/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+#from django.shortcuts import redirect, render
+#from .models import College
+#from .forms import College CreationForm
+#from django.contrib import messages
+from django.http.response import HttpResponseNotFound
+
 
 # Create your views here.
+def home(request):
+    #college = College.objects.all()
+    #return render(request, 'college/home.html', {'data': college})
+    return HttpResponse("hi")
